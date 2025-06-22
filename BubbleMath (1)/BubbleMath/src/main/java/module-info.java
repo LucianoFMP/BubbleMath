fb544348a30b8bb6d75fedb79bd001b11a78215d
luciano smanioto neto <160027894+LucianoFMP@users.noreply.github.com>
2025-06-22 19:43:31 -0300

    Add files via upload

---
+++ BubbleMath (1)/BubbleMath/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.bubblemath.bubblemath {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires java.desktop;
+    requires javafx.media;
+
+    exports com.bubblemath.bubblemath;
+    opens com.bubblemath.bubblemath to javafx.fxml;
+}
